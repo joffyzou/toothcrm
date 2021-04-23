e9a318cd20a5d265c46131c52af8136890f24847
joffy <joffyzou@qq.com>
2021-04-23 18:59:44 +0800

    个人编辑患者信息弹窗

--- resources/views/patients/edit.blade.php
+++ resources/views/patients/edit.blade.php
@@ -2,62 +2,50 @@
 
 @section('content')
 
-<form class="layui-form">
+<div class="layui-form" style="padding: 20px 70px 0 0;">
     <div class="layui-form-item">
-        <div class="layui-inline">
-            <label class="layui-form-label">姓名</label>
-            <div class="layui-input-inline">
-                <input type="text" name="phone" lay-verify="required|phone" autocomplete="off" class="layui-input">
-            </div>
+        <label class="layui-form-label">姓名</label>
+        <div class="layui-input-inline">
+            <input type="text" name="phone" lay-verify="required|phone" autocomplete="off" class="layui-input">
         </div>
     </div>
     <div class="layui-form-item">
-        <div class="layui-inline">
-            <label class="layui-form-label">电话</label>
-            <div class="layui-input-inline">
-                <input type="tel" name="email" lay-verify="email" autocomplete="off" class="layui-input">
-            </div>
+        <label class="layui-form-label">电话</label>
+        <div class="layui-input-inline">
+            <input type="tel" name="email" lay-verify="email" autocomplete="off" class="layui-input">
         </div>
     </div>
     <div class="layui-form-item">
-        <div class="layui-inline">
-            <label class="layui-form-label">平台</label>
-            <div class="layui-input-inline">
-                <select name="interest" lay-filter="aihao">
-                    <option value="0">写作</option>
-                    <option value="1">阅读</option>
-                </select>
-            </div>
+        <label class="layui-form-label">平台</label>
+        <div class="layui-input-inline">
+            <select name="interest" lay-filter="aihao">
+                <option value="0">写作</option>
+                <option value="1">阅读</option>
+            </select>
         </div>
     </div>
     <div class="layui-form-item">
-        <div class="layui-inline">
-            <label class="layui-form-label">咨询项目</label>
-            <div class="layui-input-inline">
-                <select name="interest" lay-filter="aihao">
-                    <option value="0">写作</option>
-                    <option value="1">阅读</option>
-                </select>
-            </div>
+        <label class="layui-form-label">咨询项目</label>
+        <div class="layui-input-inline">
+            <select name="interest" lay-filter="aihao">
+                <option value="0">写作</option>
+                <option value="1">阅读</option>
+            </select>
         </div>
     </div>
     <div class="layui-form-item">
-        <div class="layui-inline">
-            <label class="layui-form-label">业绩</label>
-            <div class="layui-input-inline">
-                <input type="tel" name="email" lay-verify="email" autocomplete="off" class="layui-input">
-            </div>
+        <label class="layui-form-label">业绩</label>
+        <div class="layui-input-inline">
+            <input type="tel" name="email" lay-verify="email" autocomplete="off" class="layui-input">
         </div>
     </div>
     <div class="layui-form-item">
-        <div class="layui-inline">
-            <label class="layui-form-label">来源</label>
-            <div class="layui-input-inline">
-                <input type="tel" name="email" lay-verify="email" autocomplete="off" class="layui-input">
-            </div>
+        <label class="layui-form-label">来源</label>
+        <div class="layui-input-inline">
+            <input type="tel" name="email" lay-verify="email" autocomplete="off" class="layui-input">
         </div>
     </div>
-</form>
+</div>
 
 @endsection
 
