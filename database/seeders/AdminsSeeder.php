8ac49fd539499618289b74ced147a03d85480142
joffyzou <joffyzou@icloude.com>
2021-04-26 00:53:50 +0800

    编辑ide配置文件

--- database/seeders/AdminsSeeder.php
+++ database/seeders/AdminsSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Admin;
 
 class AdminsSeeder extends Seeder
 {
@@ -13,8 +14,8 @@ class AdminsSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\Admin::factory(10)->create();
-        $admin = \App\Models\Admin::find(1);
+        Admin::factory(10)->create();
+        $admin = Admin::find(1);
         $admin->username = 'admin';
         $admin->role_id = 1;
         $admin->password = bcrypt('admin');
