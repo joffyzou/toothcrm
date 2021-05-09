7308e603ddb2018f3aa5b277af438c72dbc8572d
joffy <joffyzou@qq.com>
2021-05-09 13:55:37 +0800

    头部调整

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -2,16 +2,7 @@
     <div class="layui-logo">Toothcrm-admin</div>
     <ul class="layui-nav layui-layout-right">
         <li class="layui-nav-item">
-            <a href="javascript:;">{{ Auth::user()->username }}</a>
-{{--            <dl class="layui-nav-child">--}}
-{{--                <dd>--}}
-{{--                    <form action="{{ route('admin.logout') }}" method="POST" id="logout">--}}
-{{--                        @csrf--}}
-{{--                        @method('DELETE')--}}
-{{--                        <a href="javascript:void(0)" onclick="document.getElementById('logout').submit();return false;">退出</a>--}}
-{{--                    </form>--}}
-{{--                </dd>--}}
-{{--            </dl>--}}
+            <a href="javascript:;">{{ Auth::user()->username }}<span class="layui-badge">9</span></a>
         </li>
         <li class="layui-nav-item">
             <form action="{{ route('admin.logout') }}" method="POST" id="logout">
