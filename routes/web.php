63093a63557c706c1fe3cc776fa08159f8760d79
joffy <joffyzou@qq.com>
2021-05-24 16:05:24 +0800

    web

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
     Route::resource('users', 'UsersController');  // 管理员管理
     Route::match(['get', 'put'], 'users', 'UsersController@index')->name('users.index');   // 修改账号密码
 
-    Route::get('cespatients/lists', 'PatientsController@lists')->name('cespatients.lists'); // 患者公海
+    Route::get('patients/lists', 'PatientsController@lists')->name('patients.lists'); // 患者公海
     Route::resource('patients', 'PatientsController');  // 患者管理
 
     Route::get('users/{user}/patients', 'UsersController@patients')->name('users.patients'); // 我的患者
